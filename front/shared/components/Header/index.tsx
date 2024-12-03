fbac50fbcfe5b88725669ec6f2c574f3bd983433
yko-git <yko.iwata@gmail.com>
2024-12-03 10:11:19 +0900

    個別投稿ページ作成

--- front/shared/components/Header/index.tsx
+++ front/shared/components/Header/index.tsx
@@ -54,9 +54,9 @@ export default function Header() {
                 fill="currentColor"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 />
               </svg>
               Menu
