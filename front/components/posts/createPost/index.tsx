6183b20b2854abf48cf250618ca66b7636792c34
yko-git <yko.iwata@gmail.com>
2024-12-16 11:57:39 +0900

    定数のデータを別ファイルに切り分ける（feature-akapo_fb03）

--- front/components/posts/createPost/index.tsx
+++ front/components/posts/createPost/index.tsx
@@ -5,6 +5,7 @@ import { getMockUserToken, createPost } from "@/api/fetchData";
 import { NewPost } from "@/types";
 import Button from "@/components/shared/button";
 import SelectBox from "@/components/shared/selectBox";
+import { statusList, categories } from "@/components/shared/data";
 
 const CreatePost = () => {
   const [title, setTitle] = useState<string>("");
@@ -83,10 +84,7 @@ const CreatePost = () => {
       <div>
         <label>ステータス</label>
         <SelectBox
-          options={[
-            { value: "0", label: "下書き" },
-            { value: "1", label: "公開" },
-          ]}
+          options={statusList}
           value={status}
           onChange={handleSelect}
         />
@@ -94,11 +92,7 @@ const CreatePost = () => {
       <div>
         <label>カテゴリ</label>
         <SelectBox
-          options={[
-            { value: "1", label: "プログラミング" },
-            { value: "2", label: "キャリア" },
-            { value: "3", label: "趣味" },
-          ]}
+          options={categories}
           multiple
           value={categoryIds.map(String)}
           onChange={handleMultipleSelect}
---
+++ front/components/shared/data/index.tsx
@@ -0,0 +1,10 @@
+export const categories = [
+  { value: "1", label: "プログラミング" },
+  { value: "2", label: "キャリア" },
+  { value: "3", label: "趣味" },
+];
+
+export const statusList = [
+  { value: "0", label: "下書き" },
+  { value: "1", label: "公開" },
+];
