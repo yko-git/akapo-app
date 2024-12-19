bf544ba56b2f8d1cd0c322007834a7de9ef8ecc8
yko-git <yko.iwata@gmail.com>
2024-12-19 11:36:07 +0900

    LoginUserコンポーネント作成（feature-akapo_login）

---
+++ front/components/posts/loginUser/index.tsx
@@ -0,0 +1,53 @@
+"use client";
+import React, { useState } from "react";
+import Button from "@/components/shared/button";
+import { createLogin } from "@/api/fetchData";
+import { NewLogin } from "@/types";
+
+const LoginUser = () => {
+  const [loginId, setLoginId] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = async () => {
+    const postData: NewLogin = { loginId, password };
+    try {
+      const token = await createLogin(postData);
+
+      if (token) {
+        alert("ログインに成功しました！");
+        console.log("取得したトークン:", token);
+
+        window.location.href = "/mypage";
+      }
+    } catch (error) {
+      console.error("ログイン処理でエラーが発生しました", error);
+      alert("ログインに失敗しました");
+    }
+  };
+
+  return (
+    <div className="flex flex-col p-5 space-y-4">
+      <div>
+        <label>ログインID</label>
+        <input
+          type="text"
+          value={loginId}
+          onChange={(e) => setLoginId(e.target.value)}
+          className="border rounded p-2 w-full"
+        />
+      </div>
+      <div>
+        <label>パスワード</label>
+        <input
+          type="text"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="border rounded p-2 w-full"
+        />
+      </div>
+      <Button onClick={handleSubmit}>投稿する</Button>
+    </div>
+  );
+};
+
+export default LoginUser;
