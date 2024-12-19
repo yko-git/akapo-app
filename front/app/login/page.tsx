2f0cdbd139d2fae2450f63699ccda1349aa7e2b1
yko-git <yko.iwata@gmail.com>
2024-12-19 11:32:15 +0900

    ログインページのコンポーネント作成（feature-akapo_login）

---
+++ front/app/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginUser from "@/components/posts/loginUser";
+
+export default function Login() {
+  return (
+    <>
+      <div className="m-4 wrapper">
+        <h1 className="font-bold my-2">ログイン</h1>
+        <LoginUser />
+      </div>
+    </>
+  );
+}
