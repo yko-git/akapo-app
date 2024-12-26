bcb36b7c3ea3ac8b5e67111d21380f74ea712395
yko-git <yko.iwata@gmail.com>
2024-12-26 16:55:30 +0900

    エラーハンドリング（try catch）の処理を読み込み側に統一（feature-akapo_fb10）

--- front/components/posts/createPost/index.tsx
+++ front/components/posts/createPost/index.tsx
@@ -48,17 +48,17 @@ const CreatePost = () => {
       alert("画像を選択してください");
       return;
     }
-    if (!token) {
-      alert("tokenが取得できません");
-      return;
-    }
 
     const postData: NewPost = { title, body, status, categoryIds };
-    const postImg = await createPost(file, token, postData);
-    if (postImg) {
-      setImageUrl(postImg);
-    } else {
-      console.error("画像のアップロードまたは投稿に失敗しました");
+    try {
+      const postImg = await createPost(file, postData);
+      if (postImg) {
+        setImageUrl(postImg);
+      } else {
+        console.error("画像のアップロードまたは投稿に失敗しました");
+      }
+    } catch (error) {
+      console.error("投稿処理中にエラーが発生しました:", error);
     }
   };
 
--- front/components/posts/createUser/index.tsx
+++ front/components/posts/createUser/index.tsx
@@ -23,11 +23,15 @@ const CreateUser = () => {
     }
 
     const userData: NewUser = { loginId, name, password };
-    const postImg = await createUser(file, userData);
-    if (postImg) {
-      setImageUrl(postImg);
-    } else {
-      alert("画像のアップロードまたは投稿に失敗しました");
+    try {
+      const postImg = await createUser(file, userData);
+      if (postImg) {
+        setImageUrl(postImg);
+      } else {
+        alert("画像のアップロードまたは投稿に失敗しました");
+      }
+    } catch (error) {
+      console.error("投稿処理中にエラーが発生しました:", error);
     }
   };
 
