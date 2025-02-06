b39aade37e12cf7cd1d95562df30b4373657b0c4
yko-git <yko.iwata@gmail.com>
2025-02-06 12:44:36 +0900

    authエンドポイント用のRouter設定を追加（feature-fb_02）

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,6 @@ import { User } from "./models/user";
 import { Comment } from "./models/comment";
 import bodyParser from "body-parser";
 import passport, { hash } from "./auth";
-import jwt from "jsonwebtoken";
 import { Post } from "./models/post";
 import {
   generateExpiresAt,
@@ -19,6 +18,7 @@ import {
   updateIconSignedUrls,
   fetchComments,
 } from "./services/index";
+import authRoutes from "./routes/auth";
 
 if (!process.env.MYPEPPER || !process.env.JWT_SECRET) {
   console.error("env vars are not set.");
@@ -49,80 +49,12 @@ app.use(
 );
 app.use(bodyParser.json());
 
+app.use("/auth", authRoutes);
+
 // index
 app.get("/", (req: Request, res: Response) => {
   res.send({ message: "ok" });
 });
-
-app.post("/auth/signup", async (req, res, next) => {
-  try {
-    const hashedPassword = await hash(req);
-
-    const { user: params } = req.body;
-    const { id, loginId, name, iconUrl, authorizeToken } = params || {};
-    const user = { id, loginId, name, iconUrl, authorizeToken };
-    user.authorizeToken = hashedPassword;
-
-    const searchUser = await User.findAll({
-      where: {
-        loginId,
-      },
-    });
-
-    if (searchUser.length) {
-      return res
-        .status(400)
-        .json({ errorMessage: "ユーザー情報がすでに登録されています" });
-    }
-
-    const iconSignedUrl = await getSignedUrl({
-      ...signedURLConfig,
-      Key: iconUrl,
-    });
-
-    const userData = await User.create({
-      ...user,
-      iconSignedUrl,
-      iconUrlExpiresAt: generateExpiresAt(),
-    });
-
-    res.json({
-      user: {
-        id: userData.id,
-        loginId: userData.loginId,
-        name: userData.name,
-        iconUrl: userData.iconUrl,
-        iconSignedUrl: userData.iconSignedUrl,
-      },
-    });
-  } catch (error) {
-    console.log(error);
-    return res
-      .status(500)
-      .json({ errorMessage: "ユーザーが正しく登録できませんでした" });
-  }
-});
-
-// auth/login
-app.post(
-  "/auth/login",
-  passport.authenticate("local", {
-    session: false,
-  }),
-  (req: Request, res: Response) => {
-    try {
-      // jwtのtokenを作成
-      const user = req.user;
-      const payload = { user: req.user };
-      const token = jwt.sign(payload, `${process.env.JWT_SECRET}` as string, {
-        expiresIn: "30days",
-      });
-      res.json({ user, token });
-    } catch (err) {
-      return res.status(401).json({ errorMessage: "認証ができませんでした" });
-    }
-  }
-);
 
 // user
 app.get(
