986e6eafca4a97d0d1e4a7a64a0cbadf629a4b3c
yko-git <yko.iwata@gmail.com>
2025-02-07 11:27:03 +0900

    authのRouter設定にpassport追加（feature-fb_02）

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { createAuth, loginAuth } from "../controllers/authController";
+import passport from "passport";
 
 const router = Router();
 
 router.post("/signup", createAuth);
-router.post("/login", loginAuth);
+router.post(
+  "/login",
+  passport.authenticate("local", { session: false }),
+  loginAuth
+);
 
 export default router;
