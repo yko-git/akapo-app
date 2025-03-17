26c270e0721c7239f41308d73dadf11874dd82e8
yko-git <yko.iwata@gmail.com>
2025-03-18 06:24:02 +0900

    NavコンポーネントにLoginContextいれる（feature-akapo_signin）

--- front/components/shared/nav/index.tsx
+++ front/components/shared/nav/index.tsx
@@ -2,12 +2,15 @@
 import { jost } from "@/components/shared/font";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { useContext, useEffect } from "react";
+import { LoginContext } from "../loginContext";
 
 export interface Nav {
   login: boolean;
 }
 
-export const Nav = ({ login }: Nav) => {
+export const Nav = () => {
+  const isLoggedIn = useContext(LoginContext);
   const navs = [
     {
       name: "HOME",
@@ -28,20 +31,22 @@ export const Nav = ({ login }: Nav) => {
     <>
       {navs.map((n) => {
         const isActive = new RegExp(`^${n.link}(/.*)?$`).test(pathname);
-        return login ? (
-          <li key={n.name} className="px-4">
-            <Link
-              href={n.link}
-              className={`${jost.className} ${
-                isActive
-                  ? "border-b-2 border-[#6C9FE0] text-sm font-[15px] text-[#6C9FE0] tracking-[.2rem]"
-                  : "text-sm font-[15px] text-[#6C9FE0] tracking-[.2rem]"
-              } `}
-            >
-              {n.name}
-            </Link>
-          </li>
-        ) : null;
+        return (
+          isLoggedIn?.isLoggedIn && (
+            <li key={n.name} className="px-4">
+              <Link
+                href={n.link}
+                className={`${jost.className} ${
+                  isActive
+                    ? "border-b-2 border-[#6C9FE0] text-sm font-[15px] text-[#6C9FE0] tracking-[.2rem]"
+                    : "text-sm font-[15px] text-[#6C9FE0] tracking-[.2rem]"
+                } `}
+              >
+                {n.name}
+              </Link>
+            </li>
+          )
+        );
       })}
     </>
   );
