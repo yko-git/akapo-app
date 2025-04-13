e7f10abdb0770a74a6755eca190b3ddd858be5db
yko-git <yko.iwata@gmail.com>
2025-04-14 07:02:23 +0900

    コメント機能一旦戻す（develop）

--- front/components/posts/comments/index.tsx
+++ front/components/posts/comments/index.tsx
@@ -1,32 +1,16 @@
 "use client";
-import { useEffect, useState } from "react";
 import { jost } from "@/components/shared/font";
-// import CommentList from "@/components/posts/commentList";
+import CommentList from "@/components/posts/commentList";
+import { Comment } from "@/api/fetchData";
 import CreateComment from "@/components/posts/createComment";
-import { fetchComments, Comment } from "@/api/fetchData";
 
 interface CommentsProps {
+  comments: Comment[];
   postId: number;
   id: number;
 }
 
-export default function Comments({ postId, id }: CommentsProps) {
-  const [comments, setComments] = useState<Comment[]>([]);
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const commentList = await fetchComments({ postId: id });
-        setComments(commentList);
-      } catch (error) {
-        console.error("投稿の取得でエラーが発生しました:", error);
-      }
-    }
-
-    fetchData();
-  }, [id]);
-  const handleNewComment = (newComment: Comment) => {
-    setComments((prevComments) => [...prevComments, newComment]);
-  };
+export default function Comments({ comments, postId, id }: CommentsProps) {
   return (
     <>
       <div className="py-20 text-center bg-[#F5F8FD] -mt-8">
@@ -37,13 +21,8 @@ export default function Comments({ postId, id }: CommentsProps) {
             COMMENTS
           </h2>
           <div className="text-left">
-            {/* <CommentList  /> */}
-            <CreateComment
-              comments={comments}
-              id={id}
-              postId={postId}
-              onNewComment={handleNewComment}
-            />
+            <CommentList comments={comments} id={id} />
+            <CreateComment postId={postId} />
           </div>
         </div>
       </div>
--- front/components/posts/createComment/index.tsx
+++ front/components/posts/createComment/index.tsx
@@ -1,31 +1,19 @@
 "use client";
-import { Comment } from "@/api/fetchData";
 import React, { useState, useEffect } from "react";
 import { createComment } from "@/api/fetchData";
 import Button from "@/components/shared/button";
-import CommentList from "@/components/posts/commentList";
 
-interface CreateCommentProps {
+interface CreateComment {
   postId: number;
-  onNewComment: (comment: Comment) => void;
-  comments: Comment[];
-  id: number;
 }
 
-export default function CreateComment({
-  postId,
-  onNewComment,
-  comments,
-  id,
-}: CreateCommentProps) {
+export default function CreateComment({ postId }: CreateComment) {
   const [body, setBody] = useState<string>("");
 
   const handleSubmit = async () => {
     const postData = { body, postId };
     try {
-      const newComment = await createComment(postId, postData);
-      onNewComment(newComment); // コメントを親コンポーネントに渡して即時反映
-      setBody("");
+      await createComment(postId, postData);
     } catch (error) {
       console.error("投稿処理中にエラーが発生しました:", error);
     }
@@ -33,8 +21,6 @@ export default function CreateComment({
 
   return (
     <div className="mt-20">
-      {/* コメントリストの表示 */}
-      <CommentList comments={comments} id={id} />
       <div>
         <div className="font-bold">コメントをいれる</div>
         <textarea
