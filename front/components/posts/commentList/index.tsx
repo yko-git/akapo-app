f4a366d43aba778ab9a51e79bc38191f12d20bc1
yko-git <yko.iwata@gmail.com>
2025-04-16 09:32:34 +0900

    コメント周りstyle修正（develop）

--- front/components/posts/commentList/index.tsx
+++ front/components/posts/commentList/index.tsx
@@ -33,20 +33,31 @@ export default function CommentList({ comments, id }: CommentListProps) {
               </div>
 
               <div
-                className={`relative w-full ${isOwn ? "md:ml-8" : "md:mr-8"}`}
+                className={`relative md:min-w-[450px] ${
+                  isOwn ? "md:ml-8" : "md:mr-8"
+                }`}
               >
                 <div
-                  className={`absolute top-1/2 w-0 h-0 border-transparent md:border-[9px] transform -translate-y-1/2 ${
-                    isOwn ? "-left-4 border-r-white" : "-right-4 border-l-white"
+                  className={`absolute top-1/2 w-0 h-0 border-t-[transparent] border-b-[transparent] md:border-[9px] transform -translate-y-1/2 ${
+                    isOwn
+                      ? "-left-4 border-r-[#6c9fe0] border-l-[transparent]"
+                      : "-right-4 border-r-[transparent] border-l-[#6c9fe0]"
                   }`}
                 />
                 <div
-                  className={`py-5 px-4 rounded-md shadow-sm bg-white tracking-widest ${
-                    isOwn ? "md:mr-auto" : "md:ml-auto md:text-right"
+                  className={`py-5 px-4 border-2 border-[#6C9FE0] shadow-md rounded-md bg-white font-bold md:text-center text-sm tracking-widest ${
+                    isOwn ? "md:mr-auto" : "md:ml-auto"
                   }`}
                 >
                   {comment.body}
                 </div>
+                <div
+                  className={`absolute top-1/2 md:inline-block hidden border-solid ${
+                    isOwn
+                      ? "-left-4 [transform:translate(9px,-50%)] [border-color:transparent_#fff_transparent_transparent] [border-width:6px_7px_6px_2px]"
+                      : "-right-4 [transform:translate(-9px,-50%)] [border-color:transparent_transparent_transparent_#fff] [border-width:6px_2px_6px_7px]"
+                  }`}
+                />
               </div>
             </li>
           );
