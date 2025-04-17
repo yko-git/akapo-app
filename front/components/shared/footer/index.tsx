1006ff08286e68a983e4e42f1ffd360dc89198bf
yko-git <yko.iwata@gmail.com>
2025-04-17 09:35:09 +0900

    copyrightの西暦をDateオブジェクトのgetFullYearメソッドで実装（develop）

--- front/components/shared/footer/index.tsx
+++ front/components/shared/footer/index.tsx
@@ -1,6 +1,8 @@
 import { jost } from "@/components/shared/font";
 
 export function Footer() {
+  const date = new Date();
+  const year = date.getFullYear();
   return (
     <>
       <div className="bg-white">
@@ -8,7 +10,7 @@ export function Footer() {
           <div
             className={`${jost.className} font-bold py-8 text-center text-sm text-[#6C9FE0]`}
           >
-            &copy; 2024 - akapo.
+            &copy; {year} - akapo.
           </div>
         </footer>
       </div>
