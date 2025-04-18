a51048e6e687f6b5fc7371f530700024293eea6f
yko-git <yko.iwata@gmail.com>
2025-04-18 14:19:53 +0900

    infoのスタイル調整（develop）

--- front/components/posts/articleInfo/index.tsx
+++ front/components/posts/articleInfo/index.tsx
@@ -39,7 +39,7 @@ export default function ArticleInfo() {
       >
         NEW POST
       </h3>
-      <ul className="md:ml-24 md:text-sm text-xs space-y-4">
+      <ul className="md:ml-24 md:text-sm text-xs md:space-y-1 space-y-4">
         {data.slice(0, 3).map((item, index) => (
           <li key={index}>
             <Link
