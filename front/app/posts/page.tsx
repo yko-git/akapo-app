7a918f568a443e5c7e3dbe4ab1f0202a13c28d06
yko-git <yko.iwata@gmail.com>
2025-08-18 17:49:08 +0900

    posts（旧gallery）ページのコンポーネントを復活（pathname対策）（develop）

---
+++ front/app/posts/page.tsx
@@ -0,0 +1,9 @@
+import ArticleList from "@/components/posts/articleList";
+
+export default function PostPage() {
+  return (
+    <div className="m-4 md:mx-auto md:mb-20">
+      <ArticleList />
+    </div>
+  );
+}
