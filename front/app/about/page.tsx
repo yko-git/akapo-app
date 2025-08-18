fdc3a5e842fcdd881b0594e46e9edf6aa897d283
yko-git <yko.iwata@gmail.com>
2025-08-18 17:03:36 +0900

    aboutページからprofile要素削除（feature_akapo_designcheck）

--- front/app/about/page.tsx
+++ front/app/about/page.tsx
@@ -24,137 +24,35 @@ export default function About() {
               className="mx-auto"
             />
           </div>
-          <div className="md:pt-22 pt-20 md:text-sm text-xs md:leading-[3rem] leading-7 tracking-[.22em] md:text-center text-left　text-[#657994]">
-            <p className="mb-6 md:mb-8">
-              工作が大好きな子どもたちの作品を、ギャラリーのようにアーカイブしています。
-              <br className="hidden md:block" />
-              子どもの成長に合わせて、親子でいろんな実験が楽しめるよう、
-              <br />
-              これからもカスタマイズしていきます。
-            </p>
-          </div>
-        </div>
-        <div className="md:flex justify-center mt-40">
-          <Image
-            src="/profile/img-03.png"
-            loading="lazy"
-            alt=""
-            width={440}
-            height={230}
-            className="md:mr-10 md:mb-0 mb-10 shadow-md"
-          />
-          <Image
-            src="/profile/img-02.png"
-            loading="lazy"
-            alt=""
-            width={400}
-            height={271}
-            className="shadow-md"
-          />
-        </div>
-        <div className="text-center md:mt-20 mt-12">
-          <h2 className="font-bold">新規ユーザー登録</h2>
-          <div className="mt-5">
-            <p>
-              akapoを使用するには、
-              <br className="md:hidden" />
-              サインアップが必要です。
-            </p>
-            <div className="mt-5">
-              <Link href="/signup">
-                <Button mode="Success">サインアップ</Button>
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div className="text-center md:mt-36 md:mb-20 mt-28 mb-10">
-          <h3
-            className={`${jost.className} text-xl text-[22px] text-[#6C9FE0] tracking-[.2rem] font-bold`}
-          >
-            PROFILE
-          </h3>
-          <div className="md:flex mt-11 md:text-left text-center md:px-4 px-2">
+          <div className="text-center mt-28">
             <Image
-              src="/profile/img-01.png"
+              src="/about/img-01.png"
               loading="lazy"
               alt=""
-              width={115}
-              height={115}
-              className="mr-0 w-[115px] h-[115px] md:mr-10 inline-block"
+              width={673}
+              height={437}
             />
-            <div className="pb-12 md:pt-0 pt-7">
-              <ul className="text-left text-[#657994] tracking-[.15rem] leading-7 text-sm space-y-4">
-                <li className="flex">
-                  <span className="font-bold whitespace-nowrap text-[#2F4561] md:inline block md:mr-6 mr-4">
-                    住まい
-                  </span>
-                  東京都
-                </li>
-                <li className="md:flex">
-                  <span className="font-bold whitespace-nowrap text-[#2F4561] md:inline block md:mr-6 mr-0">
-                    紹　介
-                  </span>
-                  散歩やものづくりが好きなママエンジニアです。
-                  <br />
-                  「Akapo」は、自身の学習やインプットを目的に開発しており、React
-                  / Next.js / TypeScript を使用しています。
-                  <br />
-                  バックエンドには AWS（EC2 / RDS / S3）や Cloudflare
-                  を活用しています。
-                  <br />
-                  まだまだ開発途中のため、頻繁にアップデートを行っています！
-                </li>
-              </ul>
-            </div>
           </div>
-          <div className="border-t-2 border-[#E7E7E7] pt-10 md:px-4 px-2">
-            <ul className="text-left text-[#657994] tracking-[.15rem] leading-7 text-sm space-y-4">
-              <li className="md:flex">
-                <span className="font-bold whitespace-nowrap text-[#2F4561] md:inline block md:mr-6 mr-0">
-                  言　語
-                </span>
-                HTML, JavaScript, CSS, PHP, React, Vue, TypeScript, Next.js...
-              </li>
-              <li className="md:flex">
-                <span className="font-bold whitespace-nowrap text-[#2F4561] md:inline block md:mr-6 mr-0">
-                  TOOL
-                </span>
-                Photoshop, Illustrator, Figma, XD, VScode, git, github, Docker
-                ...
-              </li>
-              <li className="md:flex">
-                <span className="font-bold whitespace-nowrap text-[#2F4561] md:inline block md:mr-6 mr-0">
-                  経　歴
-                </span>
-                21歳のときに都内のデザイン制作会社でキャリアをスタートし、
-                <br />
-                主にWebページの作成からフロントエンド開発まで、幅広い業務を経験してきました。
-              </li>
-              <li className="md:flex">
-                <span className="font-bold whitespace-nowrap text-[#2F4561] md:inline-flex items-center block md:mr-6 mr-0">
-                  <span>学習記録</span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    width="16"
-                    height="16"
-                    className="ml-1 inline-block"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
-                    ></path>
-                  </svg>
-                </span>
-                <Link
-                  href="https://github.com/yko-git/til/"
-                  target="_blank"
-                  className="underline"
-                >
-                  https://github.com/yko-git/til
-                </Link>
-              </li>
-            </ul>
+        </div>
+
+        <div className="md:flex justify-center items-center md:mt-32 mt-16 gap-12">
+          <div className="text-sm font-semibold md:leading-[2rem] leading-7 tracking-[.22em] 　text-[#657994] max-w-[418px]">
+            <p>
+              このギャラリーは、工作が大好きな子どもたちの作品を気軽に残して、みんなで楽しめるサイトです。
+              <br />
+              ママエンジニアが自分の子どもの作品を残すために立ち上げましたが、他の方も自由にアカウントを作って作品を投稿できます。
+            </p>
+          </div>
+
+          <div className="md:text-left text-center md:mt-0 mt-5">
+            <Link href="/signup">
+              <Button
+                mode="Info"
+                className="md:py-12 md:px-20 py-6 px-10 text-white md:text-3xl font-semibold tracking-widest rounded-lg mx-auto inline-block"
+              >
+                新規ユーザー登録
+              </Button>
+            </Link>
           </div>
         </div>
       </div>
