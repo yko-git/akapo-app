7fb1ce5570d9434f6228f4e9433ed7136939dcec
yko-git <yko.iwata@gmail.com>
2025-08-19 14:45:52 +0900

    ArticleListをpageから削除（develop）

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,4 +1,3 @@
-import ArticleList from "@/components/posts/articleList";
 import Main from "@/components/posts/main";
 
 export default function Page() {
@@ -6,7 +5,6 @@ export default function Page() {
     <>
       <div className="m-4 md:mt-4 md:mx-auto md:mb-20">
         <Main />
-        <ArticleList />
       </div>
     </>
   );
