ad64e210f0a7dc6604c9af632cf5145f498d4ccd
yko-git <yko.iwata@gmail.com>
2025-08-19 14:46:21 +0900

    不要なコメントアウトを削除（develop）

--- front/components/posts/articleList/index.tsx
+++ front/components/posts/articleList/index.tsx
@@ -51,16 +51,8 @@ export default function ArticleList() {
     fetchData();
   }, [router]);
 
-  // データが取得できていない場合の表示
-  // if (status !== "success" || data === undefined) {
-  //   return <StatusInfo status={status} data={data} />;
-  // }
-
   return (
     <>
-      <div className="wrapper md:mt-20">
-        <h1 className="font-bold md:my-2 md:text-3xl text-xl">作品一覧</h1>
-      </div>
       <ul className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-y-20 gap-x-5 max-w-[1400px] mx-auto md:mt-10 px-5">
         {data?.map((item, index) => (
           <li key={index}>
