aeb1ad732f563d0736650ebcb8f6ef133c322ce4
yko-git <yko.iwata@gmail.com>
2026-01-18 15:15:25 +0900

    feat: Articleでデータ取得（fetchPosts）・ローディング・エラー管理を担うため、Mainコンポーネントを削除し、postsではなくapp/page.tsxでArticleListを読み込みに変更（develop）

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,10 +1,10 @@
-import Main from "@/components/posts/main";
+import ArticleList from "@/components/posts/articleList";
 
 export default function Page() {
   return (
     <>
       <div className="m-4 md:mt-4 md:mx-auto md:mb-20">
-        <Main />
+        <ArticleList />
       </div>
     </>
   );
