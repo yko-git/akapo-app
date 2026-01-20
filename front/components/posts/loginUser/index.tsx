18263a0d27487782a46b93eb3adbb3dfed9f8d29
yko-git <yko.iwata@gmail.com>
2026-01-20 15:08:12 +0900

    feat: loginUserを置き換える（feature/feature-store/useAuthstore）

--- front/components/posts/loginUser/index.tsx
+++ front/components/posts/loginUser/index.tsx
@@ -4,16 +4,21 @@ import Button from "@/components/shared/button";
 import { createLogin, fetchUserData } from "@/api/fetchData";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
+import { useAuthStore } from "@/stores/useAuthStore";
 
 const LoginUser = () => {
   const [loginId, setLoginId] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const router = useRouter();
+
+  const { setUserProfile } = useAuthStore();
 
   const handleSubmit = async () => {
     const postData = { loginId, password };
 
     try {
+      setIsSubmitting(true);
       const token = await createLogin(postData);
       if (!token) {
         toast.error("ログインに失敗しました");
@@ -21,11 +26,14 @@ const LoginUser = () => {
       }
 
       const userData = await fetchUserData();
+      setUserProfile(userData);
       toast.success(`ようこそ ${userData?.name} さん`);
       router.push("/mypage");
     } catch (error) {
       toast.error("ログインに失敗しました");
       console.error("ログイン処理でエラーが発生しました", error);
+    } finally {
+      setIsSubmitting(false);
     }
   };
 
@@ -60,20 +68,13 @@ const LoginUser = () => {
           <Button
             type="submit"
             onClick={handleSubmit}
+            disabled={isSubmitting}
             className="py-4 px-6 text-white text-sm font-semibold tracking-widest rounded-lg"
           >
-            ログインする
+            {isSubmitting ? "ログイン中..." : "ログイン"}
           </Button>
         </div>
       </div>
-
-      {/* <div className="mt-5 border-l-2 pl-4 leading-loose">
-        現在機能開発中のため、
-        <br />
-        ログインID：<strong>test</strong>
-        <br />
-        パスワード：<strong>test</strong>で仮ログイン可能です。
-      </div> */}
     </>
   );
 };
