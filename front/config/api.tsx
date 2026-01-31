cfb9566259b3dd2b90d4abbd8fe93820a168b07a
yko-git <yko.iwata@gmail.com>
2026-01-31 10:21:23 +0900

    feat: SIGNUPエンドポイント・コメント追加

--- front/config/api.tsx
+++ front/config/api.tsx
@@ -4,10 +4,16 @@ export const API_TIMEOUT = 30000; // タイムアウト時間（ミリ秒）
 
 // APIエンドポイント
 export const API_ENDPOINTS = {
+  // 認証
+  SIGNUP: "/auth/signup",
   LOGIN: "/auth/login",
+  // ユーザー
   USER: "/user",
   USERPOSTS: "/user/posts",
+  // 投稿
   POSTS: "/posts",
+  // コメント
   COMMENTS: "/comments",
+  // その他
   SIGNEDURL: "/signedurl",
 } as const;
