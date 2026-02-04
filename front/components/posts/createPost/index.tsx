a03ad91fea98a2436e9743cb869cad1a6cab6d50
yko-git <yko.iwata@gmail.com>
2026-02-04 09:20:25 +0900

    fix: CreatePostを試しに修正

--- front/components/posts/createPost/index.tsx
+++ front/components/posts/createPost/index.tsx
@@ -97,7 +97,7 @@ const CreatePost = () => {
         />
       </div>
       <div>
-        <label>カテゴリ</label>
+        <label>カテゴリー</label>
         <Controller
           name="categoryIds"
           control={control}
@@ -119,7 +119,7 @@ const CreatePost = () => {
       </div>
 
       <div>
-        <label>画像</label>
+        <label>作品画像</label>
         <input type="file" accept="image/*" onChange={handleFileChange} />
       </div>
       {preview && (
