5bf67afd9b165f4bd5f31ba526651141ead501f5
yko-git <yko.iwata@gmail.com>
2026-02-14 11:10:22 +0900

    feat: feature-based対応（UserPostList）

---
+++ front/features/posts/user-list/UserPostListPage.tsx
@@ -0,0 +1,11 @@
+import UserPostList from "./components/UserPostList";
+
+export default function UserPostListPage() {
+  return (
+    <>
+      <div className="m-4 md:mt-4 md:mx-auto md:mb-20">
+        <UserPostList />
+      </div>
+    </>
+  );
+}
