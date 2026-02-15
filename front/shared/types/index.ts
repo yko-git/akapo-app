6f1585ad21ba2408755dc21c0f10a89e69250f84
yko-git <yko.iwata@gmail.com>
2026-02-15 15:46:07 +0900

    feat: shared/typesを設置

---
+++ front/shared/types/index.ts
@@ -0,0 +1,20 @@
+// Component Props
+export * from "./components";
+
+// Data types
+export * from "./data";
+
+// UI types
+export * from "./ui";
+
+// Schema types
+export type {
+  User,
+  NewUser,
+  UserProfile,
+  Post,
+  NewPost,
+  Category,
+  Comment,
+  NewComment,
+} from "@/shared/schemas";
---
+++ front/shared/types/ui.ts
@@ -0,0 +1,8 @@
+// UI状態の型
+export type LoadingStatus = "loading" | "service-down" | "success" | "empty";
+
+// 選択肢の型
+export type SelectOption = {
+  value: string;
+  label: string;
+};
