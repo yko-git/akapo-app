0ac76bd2d9abc3fcf76780130d17908774c3e6bb
yko-git <yko.iwata@gmail.com>
2026-02-22 17:01:34 +0900

    fix: 読み込み修正

--- front/features/posts/create/api/createPost.ts
+++ front/features/posts/create/api/createPost.ts
@@ -1,5 +1,5 @@
 import { createPost as createPostApi } from "@/shared/api/fetchData";
-import { NewPost } from "@/schemas/post.schema";
+import { NewPost } from "@/shared/schemas";
 
 export const createPost = (file: File, data: NewPost) => {
   return createPostApi(file, data);
